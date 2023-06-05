42e49edce99db01d643eea6db8428708a1ed7cf8
Luis Anaya <brothersailorwebdev@gmail.com>
2023-06-06 01:56:15 +0200

    x

--- components/pages/peak-page.tsx
+++ components/pages/peak-page.tsx
@@ -1,19 +1,9 @@
 import Image from "next/image"
-import {
-  ClockIcon,
-  MapPinIcon,
-  MountainIcon,
-  ThermometerSnowflakeIcon,
-  ThermometerSunIcon,
-} from "lucide-react"
 
-import { Peak } from "@/types/prisma"
-import { Separator } from "@/components/ui/separator"
 import { PageContainer } from "@/components/global/page-container"
 import { PageTagline } from "@/components/global/page-tagline"
 import { PageTitle } from "@/components/global/page-title"
 import { TextWrapper } from "@/components/global/text-wrapper"
-import { PeakDetail } from "@/components/peak/peak-detail"
 import { DetailsWidget } from "@/components/peaks/details-widget"
 
 export const PeakPage = ({ peak, weather }: { peak: any; weather: any }) => {
@@ -37,36 +27,7 @@ export const PeakPage = ({ peak, weather }: { peak: any; weather: any }) => {
               className="rounded object-cover object-center"
             />
           </div>
-          {/* <Separator className="my-5 bg-branding-green" /> */}
 
-          {/* <section className="flex justify-between gap-x-5">
-            <div>
-              <PeakDetail
-                icon={<MapPinIcon className="h-4 w-4" />}
-                value={`${peak.range}`}
-              />
-            </div>
-
-            <div className="flex flex-col gap-y-2.5">
-              <PeakDetail
-                icon={<MountainIcon className="h-4 w-4" />}
-                value={peak.elevation}
-                unit="m"
-              />
-              <PeakDetail
-                icon={<ClockIcon className="h-4 w-4" />}
-                value={peak.elevation}
-              />
-              <PeakDetail
-                icon={<MountainIcon className="h-4 w-4" />}
-                value={peak.elevation}
-              />
-              <PeakDetail
-                icon={<ThermometerSnowflakeIcon className="h-4 w-4" />}
-                value={weather.main.temp_min}
-              />
-            </div>
-          </section> */}
           <DetailsWidget peak={peak} weather={weather} />
         </div>
       </section>
