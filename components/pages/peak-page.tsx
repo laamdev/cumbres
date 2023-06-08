6334d63b1de83a83c8c5f09a1f28b7b3bf9cbb22
Luis Anaya <brothersailorwebdev@gmail.com>
2023-06-08 14:31:18 +0200

    added

--- components/pages/peak-page.tsx
+++ components/pages/peak-page.tsx
@@ -13,25 +13,27 @@ export const PeakPage = ({ peak, weather }: { peak: any; weather: any }) => {
   return (
     <PageContainer>
       <PageTagline>
-        <div className="flex gap-x-1">
-          <span>{peak.county}</span>
+        <div className="flex flex-wrap gap-x-1">
+          <span>{peak.county.join(" y ")}</span>
           <span> ● </span>
-          <span>{peak.range}</span>
+          <span>{peak.province.join(" y ")}</span>
+          <span> ● </span>
+          <span>{peak.range.join(" y ")}</span>
         </div>
       </PageTagline>
       <PageTitle>{peak.name}</PageTitle>
 
       <section className="mt-10 grid gap-y-10 sm:mt-20 sm:grid-cols-5 sm:gap-x-20">
         <div className="sm:col-span-2">
-          {peak.highestPoint && (
-            <div className="flex items-center space-x-2.5 text-lg font-semibold sm:text-xl">
+          {peak.highestPoint.length > 0 && (
+            <div className="mb-5 flex items-center space-x-2.5 text-lg font-semibold sm:text-xl">
               <Avatar className="flex items-center justify-center bg-branding-green text-branding-white">
                 <MedalIcon />
               </Avatar>
               {<div>{`Punto más alto de ${highestPoint}`}</div>}
             </div>
           )}
-          <TextWrapper className="mt-5">{peak.description}</TextWrapper>
+          <TextWrapper>{peak.description}</TextWrapper>
         </div>
 
         <div className="row-start-1 sm:col-span-3 sm:row-auto">
