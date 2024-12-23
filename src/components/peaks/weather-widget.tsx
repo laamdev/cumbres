c8fe76aca376a96fa47db0c3725981b69355b0ba
Luis Anaya <brothersailorwebdev@gmail.com>
2024-12-23 18:04:56 +0100

    fix

--- src/components/peaks/weather-widget.tsx
+++ src/components/peaks/weather-widget.tsx
@@ -8,6 +8,7 @@ import { ThermometerIcon } from "@/components/icons/thermometer";
 import { CloudIcon } from "@/components/icons/cloud";
 import { DropIcon } from "@/components/icons/drop";
 import { WindIcon } from "@/components/icons/wind";
+import { Separator } from "@/components/ui/separator";
 
 interface WeatherData {
   weather: [
@@ -31,8 +32,8 @@ interface WeatherData {
 
 export const WeatherWidget = ({ weather }: { weather: WeatherData }) => {
   return (
-    <section className="grid grid-cols-5 divide-x-2 divide-branding-green rounded-xl -mt-5 z-50 relative bg-branding-yellow p-2.5 text-branding-green sm:p-5">
-      <div className="col-span-2 flex flex-col items-center justify-center text-center">
+    <section className="grid sm:grid-cols-5 sm:divide-x-2 divide-branding-green rounded-xl -mt-5 z-50 relative bg-branding-yellow p-2.5 text-branding-green sm:p-5">
+      <div className="sm:col-span-2 flex flex-col items-center justify-center text-center">
         <div className="relative aspect-square h-10 w-10 rounded-full bg-gradient-to-br from-neutral-300 to-neutral-200 sm:h-14 sm:w-14">
           <Image
             src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
@@ -45,7 +46,9 @@ export const WeatherWidget = ({ weather }: { weather: WeatherData }) => {
         </div>
       </div>
 
-      <div className="col-span-3 grid grid-cols-4 place-items-center gap-2.5">
+      <Separator className="sm:hidden mt-2.5" />
+
+      <div className="sm:col-span-3 mt-5 grid grid-cols-4 place-items-center gap-2.5">
         <DetailContainer>
           <DetailLabel>
             <ThermometerIcon className="size-7 rounded-full fill-white bg-branding-green px-1.5 sm:size-9" />
